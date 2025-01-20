502a6b9781d69a64b33f8f1ec9aad6f701ec3acf
csjunha <qkrwnsgk0303@kaist.ac.kr>
2025-01-20 09:59:29 +0900

    chore: cleanup

--- src/sim.cpp
+++ src/sim.cpp
@@ -120,8 +120,18 @@ int main(int argc, char *argv[])
     GlobalConfig &config = GlobalConfig::get_instance();
 
     po::options_description desc("Options");
-    desc.add_options()("help,h", "Print help message")("input-files", po::value<std::vector<std::string>>()->multitoken()->required(), "Input file paths (number of files should be exactly 2)")("output-file", po::value<std::string>()->required(), "Output filename")("num-vectors", po::value<uint32_t>()->default_value(DEFAULT_NUM_VECTORS), "Number of vectors to test")("vector-size", po::value<uint32_t>()->default_value(DEFAULT_VECTOR_SIZE), "Size of the input vectors")("add-pad-width", po::value<uint32_t>()->default_value(DEFAULT_ADD_PAD_WIDTH), "Padding width for addition")("adder-tree-pad-width", po::value<uint32_t>()->default_value(DEFAULT_ADDER_TREE_PAD_WIDTH), "Padding width for the adder tree")("input-exp-width", po::value<uint32_t>()->default_value(DEFAULT_INPUT_EXP_WIDTH), "Exponent width of the input vectors")("input-man-width", po::value<uint32_t>()->default_value(DEFAULT_INPUT_MAN_WIDTH), "Mantissa width of the input vectors")("result-exp-width", po::value<uint32_t>()->default_value(DEFAULT_RESULT_EXP_WIDTH), "Exponent width of the result")("result-man-width", po::value<uint32_t>()->default_value(DEFAULT_RESULT_MAN_WIDTH), "Mantissa width of the result");
-
+    desc.add_options()
+        ("help,h",                                                                                      "Print help message")
+        ("input-files",             po::value<std::vector<std::string>>()->multitoken()->required(),    "Input file paths (number of files should be exactly 2)")
+        ("output-file",             po::value<std::string>()->required(),                               "Output filename")
+        ("num-vectors",             po::value<uint32_t>()->default_value(DEFAULT_NUM_VECTORS),          "Number of vectors to test")
+        ("vector-size",             po::value<uint32_t>()->default_value(DEFAULT_VECTOR_SIZE),          "Size of the input vectors")
+        ("add-pad-width",           po::value<uint32_t>()->default_value(DEFAULT_ADD_PAD_WIDTH),        "Padding width for addition")
+        ("adder-tree-pad-width",    po::value<uint32_t>()->default_value(DEFAULT_ADDER_TREE_PAD_WIDTH), "Padding width for the adder tree")
+        ("input-exp-width",         po::value<uint32_t>()->default_value(DEFAULT_INPUT_EXP_WIDTH),      "Exponent width of the input vectors")
+        ("input-man-width",         po::value<uint32_t>()->default_value(DEFAULT_INPUT_MAN_WIDTH),      "Mantissa width of the input vectors")
+        ("result-exp-width",        po::value<uint32_t>()->default_value(DEFAULT_RESULT_EXP_WIDTH),     "Exponent width of the result")
+        ("result-man-width",        po::value<uint32_t>()->default_value(DEFAULT_RESULT_MAN_WIDTH),     "Mantissa width of the result");
     po::variables_map vm;
 
     try
@@ -209,46 +219,3 @@ int main(int argc, char *argv[])
 
     std::cout << "\nDone!\n" << std::endl;
 }
-
-// #define PATH_MAX 1024
-
-// int main()
-// {
-//     std::srand(std::time(nullptr));
-
-//     FP *vecA = (FP *)malloc(sizeof(FP) * VECTOR_SIZE);
-//     FP *vecB = (FP *)malloc(sizeof(FP) * VECTOR_SIZE);
-
-//     float acc = 0;
-
-//     for (int i = 0; i < VECTOR_SIZE; i++)
-//     {
-//         float random_floatA = gen_random_float32(-0.0001f, 0.0001f);
-//         float random_floatB = gen_random_float32(-0.0001f, 0.0001f);
-//         acc += random_floatA * random_floatB;
-
-//         uint32_t dataA = *reinterpret_cast<uint32_t *>(&random_floatA);
-//         uint32_t dataB = *reinterpret_cast<uint32_t *>(&random_floatB);
-//         uint16_t dataA_fp16 = fp32_to_fp16(dataA);
-//         uint16_t dataB_fp16 = fp32_to_fp16(dataB);
-
-//         dump_bits_hex("dataA", dataA, 32);
-//         dump_bits_hex("dataA_fp16", dataA_fp16, 16);
-
-//         vecA[i] = FP((uint64_t)dataA_fp16, INPUT_EXP_WIDTH, INPUT_MAN_WIDTH);
-//         vecB[i] = FP((uint64_t)dataB_fp16, INPUT_EXP_WIDTH, INPUT_MAN_WIDTH);
-//     }
-
-//     FP result = inner_product(
-//         vecA,
-//         vecB,
-//         VECTOR_SIZE,
-//         RESULT_EXP_WIDTH,
-//         RESULT_MAN_WIDTH);
-
-//     free(vecA);
-//     free(vecB);
-
-//     result.dump("Inner Product Result");
-//     printf("C++ FP32 Result: %.16f\n", acc);
-// }
